e7c3b7fc1a4cf82e0981d58c5d4af0dea0a29745
marcusleonas <marcus@marcusleonasharvey.com>
2026-02-27 13:25:46 +0000

    add site announcement banner

---
+++ src/components/global/announcements.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { SITE_ANNOUNCEMENT } from "../../consts";
+  let closed = $state(false);
+</script>
+
+{#if SITE_ANNOUNCEMENT.enabled}
+  {#if !closed}
+    <div>
+      <p>
+        {SITE_ANNOUNCEMENT.message}
+      </p>
+      <button onclick={() => (closed = true)}>close</button>
+    </div>
+  {/if}
+{/if}
+
+<style>
+  div {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    font-size: 14px;
+    align-items: center;
+    justify-content: center;
+
+    padding: 15px;
+    background: var(--background);
+    border: 1px solid var(--border);
+    backdrop-filter: blur(3px);
+    border-radius: 3px;
+  }
+
+  p {
+    padding: 0;
+  }
+
+  button {
+    background: var(--heading-colour);
+    border: 1px solid var(--border);
+    color: var(--foreground);
+    height: max-content;
+    padding: 4px 8px;
+    border-radius: 3px;
+    cursor: pointer;
+    font-size: 16px;
+    font-family: "Iosevka", monospace;
+  }
+
+  button:hover {
+    background: var(--glow);
+    animation: link-flicker 0.6s steps(1) forwards;
+  }
+</style>
--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,7 @@
 export const SITE_VERSION = "4.0.0";
+
+export const SITE_ANNOUNCEMENT = {
+  enabled: true,
+  message:
+    "welcome to my new site!! its a wip so things may be broken or look weird. thanks for understanding!",
+};
