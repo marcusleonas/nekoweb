ec24f0a41daa4c84becde8e79790ea6a4a8ab13a
marcusleonas <marcus@marcusleonasharvey.com>
2026-02-27 23:29:56 +0000

    add nekolink v2

---
+++ src/pages/nekolink/links.ts
@@ -0,0 +1,23 @@
+interface Link {
+  name: string;
+  img: string;
+  href: string;
+}
+
+export const nekolinks: Link[] = [
+  {
+    name: "nekolink",
+    img: "https://file.garden/aXdBx1DU1BidR1It/nekolinkv2.gif",
+    href: "https://n3bula.lol/nekolink",
+  },
+  {
+    name: "n3bula's site",
+    img: "https://file.garden/aXdBx1DU1BidR1It/n3bulalol%20website%20advert.png",
+    href: "https://n3bula.lol",
+  },
+  {
+    name: "conehead webring",
+    img: "https://file.garden/aXdBx1DU1BidR1It/conehead%20webring.png",
+    href: "https://coneheads.n3bula.lol",
+  },
+];
