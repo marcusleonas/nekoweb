36d5a3795ffa3bc04355210df7949f8f51f7b2a1
marcusleonas <marcus@marcusleonasharvey.com>
2026-02-27 13:30:27 +0000

    fix svelte maybe?????

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: "https://n3bula.lol",
-  integrations: [svelte()],
+  integrations: [svelte({ extensions: [".svelte"] })],
 });
