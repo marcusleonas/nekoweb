9c35e758d498147b82e118067a94bd4730d619ae
marcusleonas <marcus@marcusleonasharvey.com>
2026-03-08 22:23:53 +0000

    update stats script to include error handling

--- src/js/getStats.ts
+++ src/js/getStats.ts
@@ -9,21 +9,29 @@
 // updated_at: unix timestamp
 
 export async function getStats(domain: string) {
-  const req = await fetch(`https://nekoweb.org/api/site/info/${domain}`);
-  const json: APIResponse = await req.json();
-
-  const createdAt = new Date(json.created_at).toLocaleDateString("en-GB");
-  const updatedAt = new Date(json.updated_at).toLocaleDateString("en-GB");
-
   const viewsEl = document.querySelector("#sitestats-views");
   const followersEl = document.querySelector("#sitestats-followers");
   const createdAtEl = document.querySelector("#sitestats-created");
   const updatedAtEl = document.querySelector("#sitestats-updated");
 
-  if (viewsEl) viewsEl.innerHTML = json.views.toString();
-  if (followersEl) followersEl.innerHTML = json.followers.toString();
-  if (createdAtEl) createdAtEl.innerHTML = createdAt;
-  if (updatedAtEl) updatedAtEl.innerHTML = updatedAt;
+  try {
+    const req = await fetch(`https://nekoweb.org/api/site/info/${domain}`);
+    const json: APIResponse = await req.json();
+
+    const createdAt = new Date(json.created_at).toLocaleDateString("en-GB");
+    const updatedAt = new Date(json.updated_at).toLocaleDateString("en-GB");
+
+    if (viewsEl) viewsEl.innerHTML = json.views.toString();
+    if (followersEl) followersEl.innerHTML = json.followers.toString();
+    if (createdAtEl) createdAtEl.innerHTML = createdAt;
+    if (updatedAtEl) updatedAtEl.innerHTML = updatedAt;
+  } catch (error) {
+    console.error("something went wrong");
+    if (viewsEl) viewsEl.innerHTML = "please refresh the page";
+    if (followersEl) followersEl.innerHTML = "please refresh the page";
+    if (createdAtEl) createdAtEl.innerHTML = "please refresh the page";
+    if (updatedAtEl) updatedAtEl.innerHTML = "please refresh the page";
+  }
 }
 
 interface APIResponse {
