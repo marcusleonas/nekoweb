16b4cee525ec78bfcb921fa1adbf89b187e20ac1
marcusleonas <marcus@marcusleonasharvey.com>
2026-03-27 21:50:33 +0000

    add name setter/getter

--- src/components/name-setter.svelte
+++ src/components/name-setter.svelte
@@ -1,4 +1,33 @@
-<script lang="ts"></script>
+<script lang="ts">
+  function handleSubmit(e: SubmitEvent) {
+    const form = e.currentTarget as HTMLFormElement;
 
-<input type="text" id="name" name="name" placeholder="Please enter your name" />
-<input type="password" disabled placeholder="***********" />
+    const formData = new FormData(form);
+    document.cookie = `name=${formData.get("name")}`;
+
+    window.location.href = "/home";
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <div class="field">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required />
+  </div>
+  <div class="field">
+    <label for="password">Password:</label>
+    <input type="password" disabled value="***************" />
+  </div>
+
+  <button type="submit">Enter</button>
+</form>
+
+<style>
+  .field {
+    display: inline;
+    padding-right: 6px;
+  }
+  .field label {
+    font-size: 16px;
+  }
+</style>
---
+++ src/components/name.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  function getCookie(n: string) {
+    var match = document.cookie.match(new RegExp("(^| )" + n + "=([^;]+)"));
+    if (match) return match[2];
+  }
+
+  let name = $state<string>("Resident");
+
+  const returnedName = getCookie("name");
+  if (returnedName && returnedName !== "") {
+    name = returnedName;
+  }
+</script>
+
+<span>{name}</span>
